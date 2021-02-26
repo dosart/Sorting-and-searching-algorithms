fbe6a07d3701f591237cd23ea24b66fd1adcef89
Denis <Dysprosium92@ya.ru>
2021-02-26 18:01:23 +0300

    Implemented tests for linear search

--- tests/test_linear_search.py
+++ tests/test_linear_search.py
@@ -7,8 +7,29 @@ from algorithms.search import linear_search
 
 class TestLinearSearchAlgorithm:
     
-    def test_negative(self):
-        l = [1,2,3,4,5]
+    def test_negative_one(self):
+        items = [1,2,3,4,5]
         
-        assert(linear_search(l, 6) == -1)
+        assert(linear_search(items, 6) == -1)
 
+
+    def test_negative_two(self):
+        assert(linear_search([], 6) == -1)
+
+
+    def test_positive_one(self):
+        items = [1,1,2,3,4]
+        
+        assert(linear_search(items, 1) == 0)
+
+
+    def test_positive_two(self):
+        items = [1,1,2,3,4]
+        
+        assert(linear_search(items, 4) == 4)
+
+
+    def test_positive_three(self):
+        items = [1,1,2,3,4]
+        
+        assert(linear_search(items, 2) == 2)
