c632bcae5ed89095c8f9dd07d40189a32502c79f
Denis <Dysprosium92@ya.ru>
2021-02-27 23:46:57 +0300

    Tests have been redone. The tests are now parameterized

--- algorithms/search.py
+++ algorithms/search.py
@@ -18,7 +18,7 @@ def linear_search(collection, search_value):
 
 
 def find_if(collection, predicate):
-    """Return element's index if predicate is true.
+    """Return element's index if it exist in collection.
 
     Args:
         collection: iterable collection
@@ -30,4 +30,32 @@ def find_if(collection, predicate):
     for (index, element) in enumerate(collection):
         if predicate(element):
             return index
+    return -1
+
+
+def binary_search(collection, search_value):
+    """Return element's index if predicate is true.
+
+    Implementation of an iterative version of the algorithm
+
+    Args:
+        collection: iterable collection
+        search_value: search element
+
+    Returns:
+        index (int): index elementa if it exist, else -1
+    """
+    first = 0
+    last = len(collection) - 1
+
+    while first <= last:
+        mid = first + ((last - first) // 2)
+
+        if collection[mid] == search_value:
+            return mid
+        elif collection[mid] < search_value:
+            first = mid + 1
+        else:
+            last = mid - 1
+
     return -1
--- tests/test_linear_search.py
+++ tests/test_linear_search.py
@@ -3,33 +3,35 @@
 import pytest
 
 from algorithms.search import linear_search
+from algorithms.search import binary_search
 
 
-class TestLinearSearchAlgorithm:
-    
-    def test_negative_one(self):
-        items = [1,2,3,4,5]
+@pytest.mark.parametrize("function", [linear_search, binary_search])
+def test_negative_one(function):
+    items = [1,2,3,4,5]
+    assert(function(items, 6) == -1)
+
+
+@pytest.mark.parametrize("function", [linear_search, binary_search])
+def test_negative_two(function):
+    assert(function([], 6) == -1)
+
+
+@pytest.mark.parametrize("function", [linear_search, binary_search])
+def test_positive_one(function):
+    items = [1,1,2,3,4]
+    assert(function(items, 1) == 0)
+
+
+@pytest.mark.parametrize("function", [linear_search, binary_search])
+def test_positive_two(function):
+    items = [1,1,2,3,4]
         
-        assert(linear_search(items, 6) == -1)
+    assert(function(items, 4) == 4)
 
 
-    def test_negative_two(self):
-        assert(linear_search([], 6) == -1)
-
-
-    def test_positive_one(self):
-        items = [1,1,2,3,4]
+@pytest.mark.parametrize("function", [linear_search, binary_search])
+def test_positive_three(function):
+    items = [1,1,2,3,4]
         
-        assert(linear_search(items, 1) == 0)
-
-
-    def test_positive_two(self):
-        items = [1,1,2,3,4]
-        
-        assert(linear_search(items, 4) == 4)
-
-
-    def test_positive_three(self):
-        items = [1,1,2,3,4]
-        
-        assert(linear_search(items, 2) == 2)
+    assert(function(items, 2) == 2)
