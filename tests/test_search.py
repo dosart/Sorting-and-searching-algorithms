78fadf8333f568f3c80c292ff230a5a1704aa13d
Denis <Dysprosium92@ya.ru>
2021-03-02 19:45:36 +0300

    Added tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -27,12 +27,22 @@ def test_positive_one(function):
 @pytest.mark.parametrize("function", [linear_search, binary_search, exponential_search])
 def test_positive_two(function):
     items = [1,1,2,3,4]
-        
     assert(function(items, 4) == 4)
 
 
 @pytest.mark.parametrize("function", [linear_search, binary_search, exponential_search])
 def test_positive_three(function):
     items = [1,1,2,3,4]
-        
     assert(function(items, 2) == 2)
+
+
+@pytest.mark.parametrize("function", [linear_search, binary_search, exponential_search])
+def test_positive_four(function):
+    items = [1,2,3,4,5,6,7,8,9,10,11] 
+    assert(function(items, 6) == 5)
+
+
+@pytest.mark.parametrize("function", [linear_search, binary_search, exponential_search])
+def test_positive_four(function):
+    items = [1,1,1,1,1,1,1,1,0]
+    assert(function(items, 0) == 9)
