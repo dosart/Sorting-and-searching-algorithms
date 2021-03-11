dce9830b8be71936de8c5c0ac8f9407a10390a6b
Denis <Dysprosium92@ya.ru>
2021-03-11 19:08:58 +0300

    Fixed errors linter for file counting_sort.py

--- algorithms/sort/counting_sort.py
+++ algorithms/sort/counting_sort.py
@@ -4,32 +4,30 @@
 
 def counting_sort(seq):
     """Take a list of integer and sorted it.
-    
+
     Args:
-        seq (list of integer) source data
-    
+        seq (list of integer): source data
+
     Returns:
         sorted_seq (list of integer) sorted collection
     """
     if len(seq) > 1:
         return _counting_sort(seq)
-    else:
-        return seq
+    return seq
 
 
 def _counting_sort(seq):
-    counts = [0] * (max(seq) + 1)
+    counts = [0 for _ in range(max(seq) + 1)]
 
     for element in seq:
-        counts[element] = counts[element] + 1
+        counts[element] += 1
 
     for index in range(1, len(counts)):
         counts[index] = counts[index] + counts[index - 1]
 
-    sorted_seq = [0] * len(seq)
+    sorted_seq = [0 for _ in range(len(seq))]
     for element in seq:
-        position = counts[element] - 1
-        counts[element] = counts[element] - 1
-        sorted_seq[position] = element
+        counts[element] -= 1
+        sorted_seq[counts[element]] = element
 
     return sorted_seq
